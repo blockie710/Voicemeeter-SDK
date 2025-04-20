26af90540a37131141dd4e4341e6076093d07fee
blockie710 <blockie710@gmail.com>
2025-04-20 05:18:30 +0000

    Add CMake build files and installation scripts for VoicemeeterPluginHost
    - Created CMake files for building the VoicemeeterPluginHost, including object files, dependencies, and flags.
    - Implemented installation scripts to handle the installation of the plugin and its associated files.
    - Added support for cleaning up build artifacts and managing installation components.
    - Configured build settings for C++17 and optimized compilation flags.
    - Included necessary headers and source files for the plugin functionality.

---
+++ build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/Voicemeeter-SDK/PluginHost/src/aau_plugin.cpp" "/workspaces/Voicemeeter-SDK/build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/src/aau_plugin.cpp.o"
+  "/workspaces/Voicemeeter-SDK/PluginHost/src/aax_plugin.cpp" "/workspaces/Voicemeeter-SDK/build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/src/aax_plugin.cpp.o"
+  "/workspaces/Voicemeeter-SDK/PluginHost/src/main.cpp" "/workspaces/Voicemeeter-SDK/build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/src/main.cpp.o"
+  "/workspaces/Voicemeeter-SDK/PluginHost/src/plugin_interface.cpp" "/workspaces/Voicemeeter-SDK/build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/src/plugin_interface.cpp.o"
+  "/workspaces/Voicemeeter-SDK/PluginHost/src/voicemeeter_integration.cpp" "/workspaces/Voicemeeter-SDK/build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/src/voicemeeter_integration.cpp.o"
+  "/workspaces/Voicemeeter-SDK/PluginHost/src/vst3_plugin.cpp" "/workspaces/Voicemeeter-SDK/build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/src/vst3_plugin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "__linux__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../PluginHost/include"
+  "../PluginHost/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-plugin/PluginHost/CMakeFiles/VoicemeeterPluginHost.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VoicemeeterPluginHost.dir/src/aau_plugin.cpp.o"
+  "CMakeFiles/VoicemeeterPluginHost.dir/src/aax_plugin.cpp.o"
+  "CMakeFiles/VoicemeeterPluginHost.dir/src/main.cpp.o"
+  "CMakeFiles/VoicemeeterPluginHost.dir/src/plugin_interface.cpp.o"
+  "CMakeFiles/VoicemeeterPluginHost.dir/src/voicemeeter_integration.cpp.o"
+  "CMakeFiles/VoicemeeterPluginHost.dir/src/vst3_plugin.cpp.o"
+  "VoicemeeterPluginHost"
+  "VoicemeeterPluginHost.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VoicemeeterPluginHost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
